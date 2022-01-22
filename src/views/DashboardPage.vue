185fee204d3f9efde753ead79fa1101816770d99
quenti77 <quentin.ysambert@gmail.com>
2022-01-22 16:36:36 +0100

    develop: init translate i18n

--- src/views/DashboardPage.vue
+++ src/views/DashboardPage.vue
@@ -4,11 +4,13 @@
       <v-col>
         <v-card color="deep-purple darken-4">
           <v-card-title>
-            Gérer les produits
+            {{ $t('dashboard.section.products.title') }}
           </v-card-title>
           <v-card-actions class="justify-end">
             <v-btn text>
-              <span class="mr-3">Voir les produits</span>
+              <span class="mr-3">
+                {{ $t('dashboard.section.products.actions.see') }}
+              </span>
               <v-icon>mdi-arrow-right-bold</v-icon>
             </v-btn>
           </v-card-actions>
@@ -18,11 +20,13 @@
       <v-col>
         <v-card color="blue darken-4">
           <v-card-title>
-            Gérer les colis
+            {{ $t('dashboard.section.orders.title') }}
           </v-card-title>
           <v-card-actions class="justify-end">
             <v-btn text>
-              <span class="mr-3">Voir les colis</span>
+              <span class="mr-3">
+                {{ $t('dashboard.section.orders.actions.see') }}
+              </span>
               <v-icon>mdi-arrow-right-bold</v-icon>
             </v-btn>
           </v-card-actions>
@@ -32,25 +36,13 @@
       <v-col>
         <v-card color="teal darken-3">
           <v-card-title>
-            Gérer les expéditions
+            {{ $t('dashboard.section.shipments.title') }}
           </v-card-title>
           <v-card-actions class="justify-end">
             <v-btn text>
-              <span class="mr-3">Voir les expéditions</span>
-              <v-icon>mdi-arrow-right-bold</v-icon>
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-
-      <v-col>
-        <v-card color="deep-orange darken-3">
-          <v-card-title>
-            Gérer les expéditions
-          </v-card-title>
-          <v-card-actions class="justify-end">
-            <v-btn text>
-              <span class="mr-3">Voir les expéditions</span>
+              <span class="mr-3">
+                {{ $t('dashboard.section.shipments.actions.see') }}
+              </span>
               <v-icon>mdi-arrow-right-bold</v-icon>
             </v-btn>
           </v-card-actions>
--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,15 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+
+  pluginOptions: {
+    i18n: {
+      locale: 'fr',
+      fallbackLocale: 'fr',
+      localeDir: 'locales',
+      enableInSFC: false,
+      enableBridge: false
+    }
+  }
 })
