5605ca472772c0ce31edc8e2c5dd08ce9ecc6eae
quenti77 <quentin.ysambert@gmail.com>
2022-01-22 15:39:54 +0100

    develop: Init and test vuetify

---
+++ src/components/MainBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <v-app-bar dense elevation="4">
+      <v-toolbar-title>KeyProd</v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ src/views/DashboardPage.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-card color="deep-purple darken-4">
+          <v-card-title>
+            Gérer les produits
+          </v-card-title>
+          <v-card-actions class="justify-end">
+            <v-btn text>
+              <span class="mr-3">Voir les produits</span>
+              <v-icon>mdi-arrow-right-bold</v-icon>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+
+      <v-col>
+        <v-card color="blue darken-4">
+          <v-card-title>
+            Gérer les colis
+          </v-card-title>
+          <v-card-actions class="justify-end">
+            <v-btn text>
+              <span class="mr-3">Voir les colis</span>
+              <v-icon>mdi-arrow-right-bold</v-icon>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+
+      <v-col>
+        <v-card color="teal darken-3">
+          <v-card-title>
+            Gérer les expéditions
+          </v-card-title>
+          <v-card-actions class="justify-end">
+            <v-btn text>
+              <span class="mr-3">Voir les expéditions</span>
+              <v-icon>mdi-arrow-right-bold</v-icon>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+
+      <v-col>
+        <v-card color="deep-orange darken-3">
+          <v-card-title>
+            Gérer les expéditions
+          </v-card-title>
+          <v-card-actions class="justify-end">
+            <v-btn text>
+              <span class="mr-3">Voir les expéditions</span>
+              <v-icon>mdi-arrow-right-bold</v-icon>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+}
+</script>
